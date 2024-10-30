4bdb9e7bbf9287ea063c13bbc5e061e48aab9bda
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-10-30 19:55:24 +0000

    incluindo arquios js

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao (
+    'Filosofia',
+    'O que é filosofia?',
+    'Traduzindo a palavra, filosofia significa "Amor ou desejo pela Sabedoria"'
+)
+
+criaCartao (
+    'Filosofia',
+    'Quando surgiu a filosofia?',
+    'Surgiu na Grécia Antiga, aproximadamente no ano de 600 a.C.'
+)

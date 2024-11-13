1867dfbc785e8aa9917b0220b42ed1710ee2c91f
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-11-13 19:48:13 +0000

    função JS para virar cartão com click

--- perguntas.js
+++ perguntas.js
@@ -9,3 +9,15 @@ criaCartao (
     'Quando surgiu a filosofia?',
     'Surgiu na Grécia Antiga, aproximadamente no ano de 600 a.C.'
 )
+
+criaCartao (
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    'A Revolução Francesa ocorreu em 1789.'
+)
+
+criaCartao (
+    'Química',
+    'O que é um átomo?',
+    'A palavra significa "não divisível"'
+)
